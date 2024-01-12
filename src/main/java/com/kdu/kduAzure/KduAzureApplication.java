c1c15e2a024d476a5f27f34967616e96b90de0f8
kirtipadhi0kickdrum <155990562+kirtipadhi0kickdrum@users.noreply.github.com>
2024-01-13 03:35:44 +0530

    Add files via upload

---
+++ src/main/java/com/kdu/kduAzure/KduAzureApplication.java
@@ -0,0 +1,13 @@
+package com.kdu.kduAzure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KduAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KduAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kdu/kduAzure/KduAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdu.kduAzure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KduAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
